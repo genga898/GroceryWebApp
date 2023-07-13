3f47e2cb1588e9ef536355df823f564269c8db25
Emmanuel Genga <genga898@gmail.com>
2023-07-13 07:14:52 +0300

    Update Address.cs
    Making changes to the data collected by the model.

--- Groceries.Data/Models/Address.cs
+++ Groceries.Data/Models/Address.cs
@@ -5,13 +5,11 @@ namespace Groceries.Data.Models;
 public class Address
 {
     public int Id { get; set; }
-    public Users user { get; set; }
+    public Users User { get; set; }
     public string County{get; set; }
     public string Town { get; set; }
     public string Street { get; set; }
     public string Estate { get; set; }
     [DisplayName("House Number")]
     public string HouseNumber { get; set; }
-    [DisplayName("Created At")]
-    public DateTime created_at{ get; set; }
 }
