fccbdea5d5615e021d54a73e96b4b4374fe51385
Emmanuel Genga <genga898@gmail.com>
2023-07-13 13:04:08 +0300

    Add tailwind class to center the children on the page

--- GroceriesWebAppProject/ClientApp/src/components/Layout.js
+++ GroceriesWebAppProject/ClientApp/src/components/Layout.js
@@ -3,16 +3,16 @@ import { Container } from 'reactstrap';
 import { NavMenu } from './NavMenu';
 
 export class Layout extends Component {
-  static displayName = Layout.name;
+	static displayName = Layout.name;
 
-  render() {
-    return (
-      <div>
-        <NavMenu />
-        <Container tag="main">
-          {this.props.children}
-        </Container>
-      </div>
-    );
-  }
+	render() {
+		return (
+			<div>
+				<NavMenu />
+				<Container tag="main" className={"container mx-auto"}>
+					{this.props.children}
+				</Container>
+			</div>
+		);
+	}
 }
