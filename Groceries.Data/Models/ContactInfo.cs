680a393444289e845e2c99e424889eb10aadf5f7
Emmanuel Genga <genga898@gmail.com>
2023-07-13 07:17:50 +0300

    Add ContactInfo.cs
    A class containing the model for all contact information of the user

---
+++ Groceries.Data/Models/ContactInfo.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel;
+
+namespace Groceries.Data.Models;
+
+public class ContactInfo
+{
+    public int Id { get; set; }
+    public Users User { get; set; }
+    [DisplayName("Phone Number")]
+    public string Contact { get; set; }
+}
