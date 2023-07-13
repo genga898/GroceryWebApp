ed544032c893e6d4e54e231bf8a22ece30d8a739
Emmanuel Genga <genga898@gmail.com>
2023-07-13 07:15:48 +0300

    Update Cart.cs

--- Groceries.Data/Models/Cart.cs
+++ Groceries.Data/Models/Cart.cs
@@ -3,9 +3,9 @@
 public class Cart
 {
     public int Id { get; set; }
-    public Products product { get; set; }
+    public Products Product { get; set; }
     public string prodName { get; set; }
-    public int quantity{get; set; }
+    public int Quantity { get; set;}
     public Users users { get; set; }
     public string user{get; set; }
     public DateTime created_at { get; set; }
