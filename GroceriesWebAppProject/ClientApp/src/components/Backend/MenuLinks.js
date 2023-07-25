8d1cbfc5d6a02ac48abc781987c45dcbd006a380
Emmanuel Genga <genga898@gmail.com>
2023-07-25 11:37:45 +0300

    Create menu for the AdminLayout.js page

---
+++ GroceriesWebAppProject/ClientApp/src/components/Backend/MenuLinks.js
@@ -0,0 +1,40 @@
+﻿import {UserOutlined} from "@ant-design/icons";
+import {Menu} from "antd";
+import React ,{Component}from "react";
+
+export class MenuLinks extends Component {
+	static displayName = MenuLinks.name
+	
+	constructor(props) {
+		super(props);
+	}
+	
+	render() {
+		return (
+			<div className="flex flex-col justify-between">
+				<Menu theme={"dark"}
+				      mode="inline"
+				      defaultSelectedKeys={window.location.pathname}
+				      items={[
+					      {
+						      key: '1',
+						      icon: <UserOutlined/>,
+						      label: 'User Menu'
+					      },
+					      {
+						      key: '2',
+						      icon: <UserOutlined/>,
+						      label: 'User Menu'
+					      },
+					      {
+						      key: '3',
+						      icon: <UserOutlined/>,
+						      label: 'User Menu'
+					      },
+				      ]}
+				/>
+			</div>
+			
+		)
+	}
+}
